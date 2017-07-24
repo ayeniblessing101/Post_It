09f9ef87c2cb45397c6ad3848bfd351242ed01e2
Blessing <ayeniblessing32@gmail.com>
2017-07-24 13:01:20 +0100

    protect client-side routes with Higher order component

---
+++ app/client/post_it/utils/requireAuth.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function(ComposedComponent) {
+  class Authenticate extends React.Component {
+    render() {
+      return (
+        <div>
+        <div>
+      );
+    }
+  }
+  return Authenticate;
+}
