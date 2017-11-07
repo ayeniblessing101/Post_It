efb4c0d112bcb8e80d1d060942c2b531a17bd8dd
Ayeni Blessing <ayeniblessing101@gmail.com>
2017-11-07 13:30:15 +0100

    fix webpack issue

--- app/client/components/group/GroupCard.jsx
+++ app/client/components/group/GroupCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const avatar1 = require('../../images/avatar1.png');
+const avatar1 = require('../../assets/images/avatar1.png');
 
 /**
  * Renders GroupCard component
