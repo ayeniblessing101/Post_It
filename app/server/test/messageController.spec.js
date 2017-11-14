2a17a6dc3e30f0664f3373441ddf7d307a03c73b
Ayeni Blessing <ayeniblessing101@gmail.com>
2017-11-14 09:04:27 +0100

    [Chore #152835628] mirror the name of the file being tested as the filename for test files

---
+++ app/server/test/messageController.spec.js
@@ -0,0 +1,171 @@
+import * as userSeeds from '../seeders/userSeeds';
+
+require('babel-polyfill');
+
+process.env.NODE_ENV = 'test';
+
+const supertest = require('supertest');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const app = require('../app.js');
+
+const request = supertest.agent(app);
+const expect = chai.expect;
+
+const User = require('../models').User;
+const Group = require('../models').Group;
+const Message = require('../models').Message;
+
+const jwt = require('jsonwebtoken');
+
+let token;
+let fakeGroup;
+
+chai.use(chaiHttp);
+
+describe('Routes: post_message', () => {
+  // This function will run before every test to clear database
+  beforeEach((done) => {
+    User
+    .destroy({ where: {},
+      truncate: true,
+      cascade: true,
+      restartIdentity: true })
+    .then(() => User.create({
+      username: 'john',
+      email: 'john@gmail.com',
+      password: '1234',
+      phone: '2348064476683'
+    }))
+    .then((user) => {
+      Group
+      .destroy({ where: {},
+        truncate: true,
+        cascade: true,
+        restartIdentity: true })
+      .then(() => Group.bulkCreate([{
+        id: 1,
+        group_name: 'Family',
+        user_id: user.dataValues.id
+      }, {
+        id: 2,
+        group_name: 'Colleagues',
+        user_id: user.dataValues.id
+      }]))
+      .then((groups) => {
+        fakeGroup = groups[0].dataValues;
+        token = jwt.sign({ id: user.dataValues.id }, 'secret');
+        done();
+      });
+    });
+  });
+  describe('POST /api/v1/group/:id/message', () => {
+    describe('status 200', () => {
+      it('post a message', (done) => {
+        // Test's logic...
+        request.post(`/api/v1/group/${fakeGroup.id}/message`)
+        .set('Authorization', `Basic ${token}`)
+        .send(userSeeds.messagePayload)
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body.data).to.have.a.property('message_body');
+          expect(res.body.data).to.have.a.property('priority_level');
+          done(err);
+        });
+      });
+
+      it('throws an error if group does not exist', (done) => {
+        // Test logic...
+        request.post('/api/v1/group/0/message')
+        .set('Authorization', `Basic ${token}`)
+        .send(userSeeds.messagePayload)
+        .expect(404)
+        .end((err) => {
+          done(err);
+        });
+      });
+    });
+  });
+});
+
+describe('Routes: get_messages', () => {
+  // This function will run before every test to clear database
+  beforeEach(async () => {
+    await User
+      .destroy({
+        where: {},
+        truncate: true,
+        cascade: true,
+        restartIdentity: true
+      });
+
+    await Group
+      .destroy({
+        where: {},
+        truncate: true,
+        cascade: true,
+        restartIdentity: true
+      });
+
+    await Message
+      .destroy({
+        where: {},
+        truncate: true,
+        cascade: true,
+        restartIdentity: true
+      });
+
+    const user = await User.create({
+      username: 'john',
+      email: 'john@gmail.com',
+      password: '1234',
+      phone: '2348064476683'
+    });
+
+    const groups = await Group.bulkCreate([{
+      id: 1,
+      group_name: 'Family',
+      user_id: user.dataValues.id
+    }, {
+      id: 2,
+      group_name: 'Colleagues',
+      user_id: user.dataValues.id
+    }]);
+
+
+    await Message.bulkCreate([{
+      id: 1,
+      group_id: groups[0].dataValues.id,
+      priority_level: 'Normal',
+      message_body: 'Wassup',
+      user_id: user.dataValues.id
+    }, {
+      id: 2,
+      group_id: groups[1].dataValues.id,
+      priority_level: 'Normal',
+      message_body: 'How are you doing',
+      user_id: user.dataValues.id
+    }]);
+
+    fakeGroup = groups[0].dataValues;
+    token = jwt.sign({ id: user.dataValues.id }, 'secret');
+  });
+
+  describe('GET /api/v1/group/:id/messages', () => {
+    describe('get all messages', () => {
+      it('returns a list a messages', async () => {
+        // Test logic...
+        await request.get(`/api/v1/group/${fakeGroup.id}/messages`)
+          .set('Authorization', `Basic ${token}`)
+          .expect(200)
+          .then((res) => {
+            expect(res.status).to.equal(200);
+            expect(res.body).to.have.a.property('messages');
+            expect(res.body.messages).to.be.an('array');
+          });
+      });
+    });
+  });
+});
+
